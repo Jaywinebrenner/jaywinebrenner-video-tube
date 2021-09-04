3a13e5b966d8f68d1405dcbe7af509653c24bd19
Jay <jaywinebrenner@gmail.com>
2021-09-04 08:45:28 -0700

    wire up nav and style

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components'
+
+
+function Home() {
+  return (
+    <Container className="Home">
+        <h1>Home</h1>
+    </Container>
+  );
+}
+
+export default Home;
+
+const Container = styled.div`
+background-color: lightgray`
